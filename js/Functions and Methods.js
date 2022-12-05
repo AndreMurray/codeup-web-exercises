8374017881b29c77f7205dea18a5f3f60efeff9b
Andre Murray <slushymatis@gmail.com>
2022-12-04 20:05:47 -0600

    working on extra array bonuses qestions

---
+++ js/Functions and Methods.js
@@ -0,0 +1,20 @@
+"use strict"
+// Using a Set() Method: This Method sets a new object type with ES6 (es2015) that allows you to create collections of unique values. From geeksforgeeks.org.
+//// Exercise 5. Write a function named getUniqueValues that takes in an array and returns the array without any duplicates
+// function getUniqueValues(arr) {
+//     return [...new Set(arr)];
+//
+// }
+
+//You can also achieve this goal by dong indexOf() method. geeksforgeeks.org
+/* function getUniqueValue2(arr) {
+let newArray = [];
+for(let i = 0; i < arr.length; i++) {
+if(newArray.indexOf() === -1) {
+newArray.push(arr[i])
+}
+}
+}
+ */
+
+

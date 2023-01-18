eb5a737d1867e4fe38140d2a1ec2d604a9061add
Andre Murray <slushymatis@gmail.com>
2023-01-18 10:51:39 -0600

    konami code exercises

---
+++ js/index.js
@@ -0,0 +1,7 @@
+'use strict'
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+        document.querySelectorAll(".drum")[i].addEventListener("click", (() => {
+                alert("I got clicked")
+        }));
+}
+

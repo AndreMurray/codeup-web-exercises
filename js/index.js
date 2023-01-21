897d0a85283eef29e651be2abdeda1a232dec161
Andre Murray <slushymatis@gmail.com>
2023-01-20 23:06:51 -0600

    mapbox exercise

--- js/index.js
+++ js/index.js
@@ -1,7 +1,18 @@
 'use strict'
-for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
-        document.querySelectorAll(".drum")[i].addEventListener("click", (() => {
-                alert("I got clicked")
-        }));
-}
+// for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+//         document.querySelectorAll(".drum")[i].addEventListener("click", (() => {
+//                 alert("I got clicked")
+//         }));
+// }
 
+ // document.querySelector("#title").addEventListener('click', function () {
+ //         alert('this is working')
+ // })
+
+for (let i = 0; i < document.querySelectorAll('.drum').length; i++) {
+        document.querySelectorAll('.drum')[i].addEventListener('click', function () {
+                this.style.color = "white"
+        });
+}
+// audio.play()
+// let audio = new Audio("./sounds/tom-1.mp3");

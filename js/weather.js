cfed2c1728f15a1a365ffa29b309a4f6801a6269
Andre Murray <slushymatis@gmail.com>
2023-01-24 23:31:25 -0600

    weather cards are updated with SA weather data

--- js/weather.js
+++ js/weather.js
@@ -6,19 +6,67 @@ $.get('https://api.openweathermap.org/data/2.5/forecast', {
 }).done(function(data) {
     // can be used to get forecast conditions at current time in increments of 24 hours
     for (let i = 0; i < data.list.length; i += 8) {
-        console.log(data.list[i]);
+        // console.log(data.list[i]);
+        let html = `
+         <div class="col-3 border border-dark hardBox">
+            <div class="row" >
+                <div class="col-12" >${data.list[i].dt_txt}</div>
+                <hr>
+                <div class="col-12">${data.list[i].main.temp}</div>
+                <hr>
+                <div class="col-12">${data.list[i].weather[0].description}</div>
+                
+                <div class="col-12">${data.list[i].main.humidity}</div>
+                <hr>
+                <div class="col-12">${data.list[i].wind.speed}</div>
+                <hr>
+                <div class="col-12">${data.list[i].main.pressure}</div>
+            </div>
+        </div>
+        `
+        $('#forecast').append(html)
+        // console.log(data.list[i].dt_txt.value)
     }
+
 }).fail(function(jqXhr, status, error) {
     console.log(jqXhr);
     console.log(status);
     console.log(error);
 });
 
-mapboxgl.accessToken = keys.mapbox;
-let map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9',
-    zoom: 12,
-    center: [-98.652577, 29.416715]
 
-});
+
+
+
+
+
+// Map
+
+
+
+    mapboxgl.accessToken = keys.mapbox;
+    let map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9',
+        zoom: 12,
+        center: [-98.652577, 29.416715]
+
+
+    });
+
+
+   let marker =  new mapboxgl.Marker({
+       draggable: true
+   }).addTo(map).setLngLat()
+
+
+
+
+
+
+
+
+
+
+
+
